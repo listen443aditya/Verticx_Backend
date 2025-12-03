07a082ea7930602c98b8b24f458528cb3e1492f2
Aditya Kumar Singh <adityakumarsingh2121@outlook.com>
2025-12-03 15:30:41 +0530

    fix examination section add delete and modify buttons controller functions and routes

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,7 +1,16 @@
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient({
-  log: ['query', 'warn', 'error']
-});
+// const prisma = new PrismaClient({
+//   log: ['query', 'warn', 'error']
+// });
+
+// export default prisma;
+
+
+import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+// Initialize Prisma with the Accelerate extension
+const prisma = new PrismaClient().$extends(withAccelerate());
 
 export default prisma;
